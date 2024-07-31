de78a5384a659899816eada7f6656c5896acef79
Lidia <lidia.calicia@gmail.com>
2024-08-01 01:34:28 +0200

    path fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function goToTodoIfNameIsValid() {
     if (name !=="") {
         // Store name in local storage
         localStorage.setItem("userName", name);
-        window.location.href = `../todo/todo.html`;
+        window.location.href = `todo/todo.html`;
     } else {
         nameInput.classList.add("invalid");
         nameInput.setAttribute("placeholder", "please enter a name")
